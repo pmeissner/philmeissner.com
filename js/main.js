880f967553b25385b8d575589ffc9d9bd7be5623
Philip Meissner <phil@lou.pe>
2018-09-11 09:00:06 -0400

    switch

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 // $("h1").blast({ delimiter: "character" });
+$(document).ready(function() {
 
 AOS.init();
+
+$("#switch").click(function () {
+	$('h1 > span').removeClass('aos-animate');
+	setTimeout(
+  	function()
+			{
+			$("body").toggleClass("work");
+			AOS.refreshHard();
+  	}, 400);
+	console.log('clicked');
+});
+
+
+ });
+
